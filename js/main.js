f0bcbe59945f68ee62976239b2f7a9bb666ea243
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-06-14 09:45:55 +0200

    fix: put the grid inside the click button

--- js/main.js
+++ js/main.js
@@ -5,40 +5,9 @@
 // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
 
-// CREO LA GRIGLIA DINAMICA
-
-// prendo l'elemento nella quale dovrà comparire
-const container = document.querySelector(".grid");
-console.log(container);
-
-// creo la griglia interna andando a creare un elemento per ogni spazio che ho (in questo caso 10*10 = 100)
-for(i = 1; i <= 100; i++){
-
-    // creo l'elemento con la funzione
-    let cell = createElemntWithClass("div", "box")
-
-    // inserisco che ogni cella abbia il suo numero
-    cell.append(i);
-
-
-    // aggiungo un click ad ogni elemento
-    cell.addEventListener("click",
-        function(){
-            // aggiungo una classe alla cella selezionata
-            cell.classList.add("clicked")
-
-            // al click stampo in console il "testo" contenuto in ogni cella
-            console.log(cell.innerText);
-        }
-    );
-    // lo inserisco nel contenitore
-    container.append(cell);
-
-}
-
 /****************************************/
 
-// AL CLICK DEL BOTTONE FACCIO COMPARIRE LA GRIGLIA
+// AL CLICK DEL BOTTONE CREO LA GRIGLIA E LA FACCIO COMPARIRE
 
 
 // prendo il bottone
@@ -48,6 +17,37 @@ const btn = document.querySelector(".start_game");
 
 btn.addEventListener("click", 
     function(){
+        
+        // CREO LA GRIGLIA DINAMICA
+
+        // prendo l'elemento nella quale dovrà comparire
+        const container = document.querySelector(".grid");
+        console.log(container);
+
+        // creo la griglia interna andando a creare un elemento per ogni spazio che ho (in questo caso 10*10 = 100)
+        for(i = 1; i <= 100; i++){
+
+            // creo l'elemento con la funzione
+            let cell = createElemntWithClass("div", "box")
+
+            // inserisco che ogni cella abbia il suo numero
+            cell.append(i);
+
+
+            // aggiungo un click ad ogni elemento
+            cell.addEventListener("click",
+                function(){
+                    // aggiungo una classe alla cella selezionata
+                    cell.classList.add("clicked")
+
+                    // al click stampo in console il "testo" contenuto in ogni cella
+                    console.log(cell.innerText);
+                }
+            );
+            // lo inserisco nel contenitore
+            container.append(cell);
+
+        }
         container.classList.add("show");
     }
 );
